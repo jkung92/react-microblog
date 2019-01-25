fb1ddf1ce8600992ad8d27bb01ff52ecaf3ebaa1
Jonathan <jkung92@gmail.com>
2019-01-24 17:17:16 -0800

    updated add comment

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -16,6 +16,12 @@ const INITIAL_STATE = { blogPosts: {} };
 //     }
 //  }
 
+function deepCopy(obj) {
+  const str = JSON.stringify(obj);
+  const copy = JSON.parse(str);
+  return copy;
+}
+
 function rootReducer(state = INITIAL_STATE, action) {
   console.log('reducer ran; state & action:', state, action);
 
@@ -48,16 +54,11 @@ function rootReducer(state = INITIAL_STATE, action) {
     }
     case ADD_COMMENT: {
       let commentId = uuid();
-      let newState = { ...state };
-      // Make a copy of state, with this copy, change the copy and return it
-      let newBlogPosts = { ...state.blogPosts };
-      let newPost = { ...state.blogPosts.postId };
-      // Copy of state.blogPosts
-      // create a comments key inside of
-      newState.blogPosts = newBlogPosts;
-
-      newState.blogPosts[action.id] = action.post;
-      return newState;
+      let newComment = { [commentId]: action.comment };
+      const copy = deepCopy(state);
+      copy.blogPosts[action.postId].comments.push(newComment);
+      console.log('copy in add comment:', copy);
+      return copy;
     }
     case DELETE_COMMENT: {
       return state;
