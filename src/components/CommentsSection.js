c5dee37b0b8ed89156eeebfb501b8c825f5cdc0c
Jennifer Enck <jenniferenck@mac.com>
2019-01-25 07:49:46 -0800

    rewired comments to fix bug that now displays all comments

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -4,16 +4,16 @@ import CommentForm from './CommentForm';
 
 class CommentsSection extends Component {
   render() {
-    console.log(`These are props inside CommentsSection`, this.props);
     const comments = this.props.blogPosts[this.props.postId].comments;
+    console.log(`These are comment inside CommentsSection`, comments);
     return (
       <div>
         <h3>Comments: </h3>
         {comments.map(c => (
           <Comment
-            comment={c.comment}
-            key={Object.keys(c)[0]}
-            commentId={Object.keys(c)[0]}
+            comment={c.text}
+            key={c.commentId}
+            commentId={c.commentId}
             deleteComment={this.props.deleteComment}
           />
         ))}
