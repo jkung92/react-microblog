a4f5cbf35998e8ba7ee36d1f2c57cad1780b8b00
Jennifer Enck <jenniferenck@mac.com>
2019-01-25 12:38:42 -0800

    added new action type for post details

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -3,6 +3,7 @@ export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const EDIT_POST = 'EDIT_POST';
 export const LOAD_POSTS = 'LOAD_POSTS';
+export const LOAD_POST_DETAILS = 'LOAD_POST_DETAILS';
 
 // COMMENTS
 export const ADD_COMMENT = 'ADD_COMMENT';
