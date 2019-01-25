ceecfa8d6b40305021e81b67c566f2aef77a4d5d
Jennifer Enck <jenniferenck@mac.com>
2019-01-25 12:39:57 -0800

    added component didmount to post detail container

--- src/containers/PostDetailContainer.js
+++ src/containers/PostDetailContainer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import PostDetail from '../components/PostDetail';
 import { connect } from 'react-redux';
-import { editPost, deletePost } from '../actionCreator';
+import { editPost, deletePost, getPostDetailsFromApi } from '../actionCreator';
 
 class PostDetailContainer extends Component {
+  componentDidMount() {
+    this.props.getPostDetailsFromApi(this.props.match.params.postId);
+  }
+
   render() {
     return (
       <div>
@@ -18,14 +22,16 @@ class PostDetailContainer extends Component {
 // map state to props to get individual post details (object of post with comments inside)
 function mapStateToProps(state) {
   return {
-    blogPosts: state.blogPosts
+    blogPosts: state.blogPosts,
+    currPost: state.currPost
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     editPost: (post, id) => dispatch(editPost(post, id)),
-    deletePost: id => dispatch(deletePost(id))
+    deletePost: id => dispatch(deletePost(id)),
+    getPostDetailsFromApi: id => dispatch(getPostDetailsFromApi(id))
   };
 }
 
